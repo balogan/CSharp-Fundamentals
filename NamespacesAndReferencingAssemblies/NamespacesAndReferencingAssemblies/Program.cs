45a68b4153c1fa914f1aeba18727211853838a8f
Blake Logan <balogan@ua.edu>
2015-07-15 07:17:17 -0400

    Namespaces And Referencing Assemblies
    Understanding Namespaces and Adding References to Assemblies

---
+++ NamespacesAndReferencingAssemblies/NamespacesAndReferencingAssemblies/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespacesAndReferencingAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.IO.StreamReader myStreamReader = new System.IO.StreamReader();
+
+            //StreamReader myStreamReader = new StreamReader();
+
+            //Need bob.dll file
+            /*
+            Bob bob = new Bob();
+
+            string html = bob.Lookup("http://www.learnvisualstudio.net");
+
+            Console.WriteLine();
+            Console.ReadLine();
+            */
+        }
+    }
+}
