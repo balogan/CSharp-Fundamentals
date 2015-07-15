7ef7488d28a711202db839851582af0240cc3815
Blake Logan <balogan@ua.edu>
2015-07-15 13:05:13 -0400

    Add previous lessons
    Upload all previous lessons from prior videos

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            */
+ 
+            //string myFirstName = "Blake";
+
+            var myFirstName = "Blake";
+
+            Console.WriteLine(myFirstName);
+
+            Console.ReadLine();
+        }
+    }
+}
